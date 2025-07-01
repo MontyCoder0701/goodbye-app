6ddf4a229e80460cf14c7329c6fc3071d485d531
Soo <soojlee0106@naver.com>
2025-07-01 20:35:15 +0900

    fix: apply color theme

--- lib/theme.dart
+++ lib/theme.dart
@@ -3,15 +3,26 @@ import 'package:flutter/material.dart';
 class CustomTheme {
   static final light = ThemeData(
     colorScheme: ColorScheme.fromSeed(
-      seedColor: Colors.deepPurple,
+      seedColor: const Color(0xFFF0BB78),
       brightness: Brightness.light,
+      primary: const Color(0xFFF0BB78),
+      secondary: const Color(0xFF543A14),
+      onPrimary: Colors.white,
+      onSecondary: Colors.white,
+      onSurface: Colors.black,
     ),
   );
 
   static final dark = ThemeData(
     colorScheme: ColorScheme.fromSeed(
-      seedColor: Colors.deepPurple,
+      seedColor: const Color(0xFFF0BB78),
       brightness: Brightness.dark,
+      primary: const Color(0xFFF0BB78),
+      secondary: const Color(0xFF543A14),
+      surface: const Color(0xFF131010),
+      onPrimary: Colors.black,
+      onSecondary: Colors.white,
+      onSurface: Colors.white,
     ),
   );
 }
